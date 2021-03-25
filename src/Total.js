d094f43e7dd3219dce7dbc58b730a1c079aeda17
Luis Huamani <luis.huamani.c@tecsup.edu.pe>
2021-03-24 23:23:47 -0500

    cambios finales

--- src/Total.js
+++ src/Total.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-const Total = (props) => {
-  return(
-    <>
-      <p>Number of exercises {props.total}</p>
-    </>
-  )
-}
+const Total = ({parts}) => {
+    const amount = (item) => {
+      return item.exercises
+    }
+  
+    const sum = (prev, next) => {
+      return prev + next
+    }
+  
+    const total = parts.map(amount).reduce(sum)
+    return(
+      <div>
+          {parts.map}
+            <p>Number of exercises {total} </p>
+      </div>  
+    )
+    }
 
 export default Total
--- src/index.js
+++ src/index.js
@@ -5,15 +5,29 @@ import Header from "./Header"
 import Total from "./Total"
 
 const App = () => {
-  const course = 'Half Stack application development'
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content part1={part1} exercises1={exercises1}
-               part2={part2} exercises2={exercises2}
-               part3={part3} exercises3={exercises3}/>
-      <Total total={exercises1 + exercises2 + exercises3}/>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
